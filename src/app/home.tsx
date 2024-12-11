fa9eaa0f55ff1287f43b2b96b71b8e50b64f2e1b
Paulo André <pauloandre0011@gmail.com>
2024-12-11 01:21:30 -0300

    Add fetchCategories

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1,9 +1,29 @@
-import { Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Alert, View } from 'react-native';
+
+import { api } from '@/services/api';
  
 export default function Home() {
+  const [categories, setCategories] = useState();
+
+  async function fetchCategories() {
+    try {
+      const { data } = await api.get("/categories");
+
+      setCategories(data);
+    } catch (error) {
+      console.log(error);
+      Alert.alert('Categorias', 'Não foi possível carregar as categorias.');
+    }
+  }
+
+  useEffect(() => {
+    fetchCategories()
+  }, []);
+
   return (
-    <View style={{ flex: 1, padding: 40, gap: 32 }}>
-      <Text>Home</Text>
+    <View style={{ flex: 1 }}>
+
     </View>
   );
 }
